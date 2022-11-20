d3d4b53f67816f2ae21e17c5500dd9c1a2f9aa99
Javier Nova <javiernovaestrella07@gmail.com>
2022-11-20 15:19:15 -0600

    update

--- Solution.py
+++ Solution.py
@@ -32,17 +32,6 @@ def checkInput(digits):
       
   
 main()
-#913520451033444232364
-#print(i)
-#913520334412451033444123970001112334451334410101
-'''The keypad has ten keys, labeled from '0' to '9'.
-The security engine will unlock the lock when it finds the correct
-un-locking access code in the input string. Likewise, the security engine will
-lock the lock when it finds the correct locking access code in the input string.
 
-For example, let's assume that the un-locking code is 33441. If the user
-enters the string 91352033441245, then the lock will be unlocked as soon as
-the engine finds the last correct letter of the access code in the input string.
-The engine will lock the lock again when the user enters the locking code.
-Assuming that the locking code is 33444 then the lock will be locked and unlocked as described below:'''
+
 
